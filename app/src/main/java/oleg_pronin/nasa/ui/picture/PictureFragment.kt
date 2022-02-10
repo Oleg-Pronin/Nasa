569f9e3955efc4e9f3b1ce6ea139af75907def63
Oleg Pronin <o.pronin@qsoft.ru>
2022-02-10 19:32:48 +0300

    Добавление BottomSheetBehavior

--- app/src/main/java/oleg_pronin/nasa/ui/picture/PictureFragment.kt
+++ app/src/main/java/oleg_pronin/nasa/ui/picture/PictureFragment.kt
@@ -1,12 +1,15 @@
 package oleg_pronin.nasa.ui.picture
 
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import oleg_pronin.nasa.R
 import oleg_pronin.nasa.State
 import oleg_pronin.nasa.databinding.FragmentPictureBinding
@@ -18,25 +21,34 @@ class PictureFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: PictureContract.ViewModal by viewModels<PictureViewModel>()
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPictureBinding.inflate(inflater, container, false)
-        
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initViewModel(viewModel)
+        initUIEvent()
+        initViewModel()
+        setBottomSheetBehavior(binding.bottomSheetInclude.bottomSheetContainer)
 
         viewModel.getPictureOfTheDay()
     }
 
-    private fun initViewModel(viewModel: PictureContract.ViewModal) {
+    private fun initUIEvent() {
+        binding.imageApod.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
+
+    private fun initViewModel() {
         viewModel.pictureLiveData.observe(viewLifecycleOwner) { renderData(it) }
     }
 
@@ -46,12 +58,17 @@ class PictureFragment : Fragment() {
                 val apod = State.data as APOD
 
                 apod.url?.let {
-                    Glide
-                        .with(this)
-                        .load(apod.url)
-                        .error(R.drawable.ic_load_error_photo)
-                        .placeholder(R.drawable.ic_no_photo)
-                        .into(binding.imageApod)
+                    binding.let {
+                        Glide
+                            .with(this)
+                            .load(apod.url)
+                            .error(R.drawable.ic_load_error_photo)
+                            .placeholder(R.drawable.ic_no_photo)
+                            .into(it.imageApod)
+
+                        it.bottomSheetInclude.bottomSheetDescriptionHeader.text = apod.title
+                        it.bottomSheetInclude.bottomSheetDescription.text = apod.explanation
+                    }
                 }
             }
             is State.Loading -> {
@@ -62,7 +79,12 @@ class PictureFragment : Fragment() {
             }
         }
     }
-    
+
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
     companion object {
         fun newInstance() = PictureFragment()
         private var isMain = true
